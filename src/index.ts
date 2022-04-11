c9b67dd885ae6cf774da477480c0e0d552c83328
VJ9X <69524036+VJ9X@users.noreply.github.com>
2022-04-11 16:05:40 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const bot = new ds.Client({
 // login the bot and do initializations
 bot.login(envFile.TOKEN).then(() => {
     console.log(`Logged in as ${bot.user.tag}!\n\nTip: press CTRL+C to exit program.\n`)
-    bot.user.setPresence({game: {name: 'Your mom', type: 'WATCHING'}, status: 'online'})
+    bot.user.setPresence({ activities: {name: 'Your mom', type: 'WATCHING'}, status: 'online'})
 })
 
 // coolest activity code end right here
