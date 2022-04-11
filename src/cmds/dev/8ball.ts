88f79b7104d5d1e51bc91b4ab9cfe9bc7c2cd7cd
Raf <60671867+Rafplayz@users.noreply.github.com>
2022-04-11 12:45:43 -0400

    endr please rename your exports

--- src/cmds/dev/8ball.ts
+++ src/cmds/dev/8ball.ts
@@ -1,10 +1,10 @@
 /**  just saying this is the most amazing 8ball command you will ever see */
 import * as ds from 'discord.js'
 
-var answers = ['no lol', 'yeah sure', 'idk lmao', 'what', 'try later', 'maybe /j', '??? lmao i dont have all these answers', 'idk you tell me', 'uh huh', 'probably not sorry...', 'i dont think i asked actually']
-var answer = answers[Math.floor(Math.random() * answers.length)];
+let answers = ['no lol', 'yeah sure', 'idk lmao', 'what', 'try later', 'maybe /j', '??? lmao i dont have all these answers', 'idk you tell me', 'uh huh', 'probably not sorry...', 'i dont think i asked actually']
+let answer = answers[Math.floor(Math.random() * answers.length)];
 
 /** i lied i actually have no idea what im doing but i think this works :thumbs_up: thanks Google */
-export async function ping(msg: ds.Message<boolean>) {
+export async function 8ball(msg: ds.Message<boolean>) { // endr please rename your functions
     let newmessage = await msg.reply(answer);
 }
