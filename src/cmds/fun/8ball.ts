c078f63c9c4af7dbccda39244fb243e08a061505
Rafplayz <60671867+Rafplayz@users.noreply.github.com>
2022-04-11 15:28:28 -0400

    bruh

--- src/cmds/fun/8ball.ts
+++ src/cmds/fun/8ball.ts
@@ -1,10 +1,9 @@
 /*  just saying this is the most amazing 8ball command you will ever see */
 import * as ds from 'discord.js'
-
-let answers = ['no lol', 'yeah sure', 'idk lmao', 'what', 'try later', 'maybe /j', '??? lmao i dont have all these answers', 'idk you tell me', 'uh huh', 'probably not sorry...', 'i dont think i asked actually']
-let answer = answers[Math.floor(Math.random() * answers.length)];
-
 /* i lied i actually have no idea what im doing but i think this works :thumbs_up: thanks Google */
-export async function ball8(msg: ds.Message<boolean>) { // endr please rename your functions 
+export async function ball8(msg: ds.Message<boolean>) { // endr please rename your functions
+    // endr ur so dumb
+    let answers = ['no lol', 'yeah sure', 'idk lmao', 'what', 'try later', 'maybe /j', '??? lmao i dont have all these answers', 'idk you tell me', 'uh huh', 'probably not sorry...', 'i dont think i asked actually']
+    let answer = answers[Math.floor(Math.random() * answers.length)];
     let newmessage = await msg.reply(answer);// how about you go rename some bitches ooooh gottem
 }
