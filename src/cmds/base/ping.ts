e903ea84622f2be871153ecc0130a499f880bb3c
Rafplayz <60671867+Rafplayz@users.noreply.github.com>
2022-04-10 22:21:31 -0400

    does it work

--- src/cmds/base/ping.ts
+++ src/cmds/base/ping.ts
@@ -1,3 +1,18 @@
-export default function() {
-    
+import * as ds from 'discord.js'
+export default async function(msg: ds.Message<boolean>) {
+    let start = Date.now()
+    let latency = <number><unknown>msg.createdAt - start
+    let embed = new ds.MessageEmbed()
+        .setFields([
+            { name: "API Info Receive Latency", value: `${latency}ms` },
+            { name: "API Total Latency", value: "Calculating" }
+        ])
+    let newmessage = await msg.reply({ embeds: [embed] })
+    await newmessage.edit({
+            embeds: [new ds.MessageEmbed()
+                .setFields([
+                    { name: "API Info Receive Latency", value: `${latency}ms` },
+                    { name: "API Total Latency", value: `${newmessage.createdAt.getTime() - start}ms` },
+                ])]
+        })
 }
