76ace795a2e258e1eab285823cf92dba0f1fa168
actions-user <action@github.com>
2022-04-12 15:48:26 +0000

    build: 0270b36eb89815fd793a713ad4b40db65a4c9968

---
+++ js/cmds/fun/rng.js
@@ -0,0 +1,18 @@
+export async function rng(msg, args) {
+    try {
+        let num = args[0];
+        let random = Math.floor(Math.random() * num);
+        if (num === undefined) {
+            msg.channel.send('You need to give a maximum number!');
+            return;
+        }
+        switch (args?.[1]) {
+            default:
+                break;
+        }
+        msg.channel.send(random.toString());
+    }
+    catch (err) {
+        msg.reply("Something went wrong while trying to get a random number for SOME reason");
+    }
+}
